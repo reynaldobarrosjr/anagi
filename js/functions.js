3b11f676ab35924942eb1fc0a67165abe3143b95
Reynaldo <jamjrs@gmail.com>
2011-10-06 16:45:10 -0300

    refactory home my branch

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+		
+	
        //Full Box Sliding (Hidden to Visible)
 	   $('.block').hover(function(){
 	        $(".slider", this).stop().animate({left:'0px'},{queue:false,duration:160});
 	        }, function() {
 	        $(".slider", this).stop().animate({left:'140px'},{queue:false,duration:160});
-	        });	     
+	        });	  
+	        
+	        
+	      
  });
