6f5f17b7ddbdc9e421ce36ae492efaa03c2e3aca
Reynaldo <jamjrs@gmail.com>
2011-12-15 12:03:54 -0200

    Revert "dont know"
    This reverts commit e0d2452f52e5bc8603b9ca9f71dc18060cc73ffb.

---
+++ js/functions1600.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	 
+	
+       //Full Box Sliding (Hidden to Visible)
+	   $('.block').hover(function(){
+	        $(".slider", this).stop().animate({left:'0px'},{queue:false,duration:160});
+	        }, function() {
+	        $(".slider", this).stop().animate({left:'50px'},{queue:false,duration:160});
+	        });	  
+	        
+		// set opacity to nill on page load
+		$("#menu li span").css("opacity","0");
+		// on mouse over
+		$("#menu li span").hover(function () {
+			// animate opacity to full
+			$(this).stop().animate({
+				opacity: 1
+			}, 'fast');
+		},
+		// on mouse out
+		function () {
+			// animate opacity to nill
+			$(this).stop().animate({
+				opacity: 0
+			}, 'fast');
+		});        
+	      
+ });
